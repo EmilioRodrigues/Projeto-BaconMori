8b0a6801277b5c359c17701d37eac3c52d3bba7a
Emilio Barbosa <emilio.projetos@gmail.com>
2024-10-22 18:18:31 -0300

    função 'mapear' concluida

--- scripts.js
+++ scripts.js
@@ -1,22 +1,49 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
 
 let myLi = ""
 
 
-function showAll() {
-    menuOptions.forEach((product) => {
+function showAll(productsArray) {
+
+   let myLi = "" 
+   
+    productsArray.forEach((product) => {
         myLi +=
             `   <li>
                     <img src=${product.src}>
                     <p>${product.name}</p>
                     <p class="item-price">R$  ${product.price}</p>
                 </li>
-        `
+            `
     })
 
     list.innerHTML = myLi
 }
 
+function mapAllItems(){
+    const newPrices = menuOptions.map ((product) => ({
+        // name:   product.name,
+        // price: product.price * 0.9,
+        // vegan: product.vegan,
+        // src: product.src
 
-buttonShowAll.addEventListener('click', showAll)
+        // usando spread operator
+        ...product, //comando SPREAD - espalha o array mas só altera o que vc determinar 
+        price: product.price * 0.9, // alterado somente o preço dando desconto de 10%
+
+    }))
+
+    showAll(newPrices)
+
+
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+
+buttonMapAll.addEventListener('click', mapAllItems)
+
+
+
+
